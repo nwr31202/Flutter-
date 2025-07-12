194e7cc8383c852a18de7934a12718ed655daace
nwr31202 <kkaa18515@gmail.com>
2025-07-12 16:43:20 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/example/flutterapp/MainActivity.java
+++ android/app/src/main/java/com/example/flutterapp/MainActivity.java
@@ -1,1 +1,7 @@
-package com.example.flutterapp; public class MainActivity {}
+package com.example.flutterapp;
+
+import io.flutter.app.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
+
